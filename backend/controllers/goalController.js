63dd1d980c6a754f123a31d4466f678e5fdf2f6c
johnsonjason099 <johnsonjason099@gmail.com>
2022-11-22 12:34:21 -0700

    first commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -8,7 +8,7 @@ const Goal = require('../models/goalModel')
 const getGoals = asyncHandler(async (req, res) => {
     const goals = await Goal.find()
 
-        res.status(200).json({message: 'Get goals' })
+        res.status(200).json(goals)
 })
 
 //@desc   Set goal
@@ -21,7 +21,7 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
 const goal = await Goal.create({
-    text: req.body.text
+    text: req.body.text,
 })
 
     res.status(200).json(goal)
--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,11 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false}))
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/goals', require('./routes/goalRoutes')),
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
-app.listen(port, ()=> console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
